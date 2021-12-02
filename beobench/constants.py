f2784bf418b8744d5c3865b84c3895a0e4d3b3b7
rdnfn <75615911+rdnfn@users.noreply.github.com>
2021-12-02 14:19:17 +0000

    aux: refactor constants

--- beobench/constants.py
+++ beobench/constants.py
@@ -13,7 +13,11 @@ DEFAULT_INSTALL_PATH = pathlib.Path("/opt/beobench")
 BOPTEST_REPO_URL = "https://github.com/ibpsa/project1-boptest.git"
 BOPTEST_REPO_NAME = "project1-boptest"
 BOPTEST_COMMIT = "703daf26a9e9e18de7141c021cbfe36aa3578ea8"
-BOPTEST_PIP_DEP = ["flask-restful==0.3.9", "pandas==1.3.4", "flask_cors==3.0.10"]
+BOPTEST_PIP_DEP = [
+    "flask-restful==0.3.9",
+    "pandas==1.3.4",
+    "flask_cors==3.0.10",
+]
 
 BOPTEST_GYM_REPO_URL = "https://github.com/ibpsa/project1-boptest-gym.git"
 BOPTEST_GYM_REPO_NAME = "project1-boptest-gym"
