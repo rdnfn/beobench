581997a219bd458b13aca3feed4847584dc8dcdb
rdnfn <75615911+rdnfn@users.noreply.github.com>
2021-12-07 16:02:49 +0000

    aux: restructure package into integrations, method and problems
    And also add better support for parallel testcase containers

--- beobench/integration/boptest.py
+++ beobench/integration/boptest.py
@@ -3,6 +3,8 @@
 import pathlib
 import subprocess
 import time
+import uuid
+import docker
 
 from beobench.constants import DEFAULT_INSTALL_PATH
 
@@ -29,7 +31,7 @@ def build_testcase(
 
 def run_testcase(
     testcase: str = "testcase1",
-    local_port: int = 5000,
+    local_port: int = 0,
     install_path: pathlib.Path = DEFAULT_INSTALL_PATH,
     add_wait_time: bool = True,
 ) -> str:
@@ -46,21 +48,24 @@ def run_testcase(
             command the API is immidiately accessed. Defaults to True.
 
     Returns:
+        str: name of container
         str: url of testcase API
     """
 
     img_name = f"boptest_{testcase}"
+    unique_id = uuid.uuid4().hex[:6]
+    container_name = f"{img_name}_{unique_id}"
     ip_plus_port = f"127.0.0.1:{local_port}"
-    url = f"http://{ip_plus_port}"
 
     # In order to be able to change the port
     # this command is executed directly
     # without the using the make file.
+    print(f"Creating boptest testcase in container named '{container_name}'...")
     args = [
         "docker",
         "run",
         "--name",
-        f"{img_name}",
+        f"{container_name}",
         "--rm",
         # "-it",
         "-p",
@@ -81,24 +86,29 @@ def run_testcase(
         # Allow for the docker image to launch
         time.sleep(5)
 
-    return url
+    # Getting the API port on the host machine.
+    # This is necessary because, if local_port is set to 0,
+    # a random port is allocated.
+    client = docker.from_env()
+    container = client.containers.get(container_name)
+    host_ip = container.ports["5000/tcp"][0]["HostIp"]
+    host_port = container.ports["5000/tcp"][0]["HostPort"]
+    url = f"http://{host_ip}:{host_port}"
+
+    print(f"Container created. The boptest API is exposed at '{url}'.")
+
+    return container_name, url
 
 
-def stop_testcase(
-    testcase: str = "testcase1", install_path: pathlib.Path = DEFAULT_INSTALL_PATH
-) -> None:
-    """Stop currently running testcase.
+def stop_container(container_name: str) -> None:
+    """Stop docker container.
 
     Args:
-        testcase (str, optional): testcase to stop. Defaults to "testcase1".
-        install_path (pathlib.Path, optional): path of beobench installation.
-            Defaults to DEFAULT_INSTALL_PATH.
+        container_name (str): name of docker container to stop.
     """
-
-    subprocess.check_call(
-        ["make", "stop", f"TESTCASE={testcase}"],
-        cwd=_get_boptest_path(install_path),
-    )
+    client = docker.from_env()
+    container = client.containers.get(container_name)
+    container.stop(timeout=0)
 
 
 def _get_boptest_path(install_path: pathlib.Path) -> pathlib.Path:
