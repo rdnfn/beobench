174acef970acff4d0a6688ac0b2bbd930d3b211b
rdnfn <75615911+rdnfn@users.noreply.github.com>
2021-12-23 02:01:44 +0000

    aux: let containers remove themselves when stopped

--- beobench/experiment/scheduler.py
+++ beobench/experiment/scheduler.py
@@ -89,10 +89,12 @@ def run_experiments_from_cli(
         args = [
             "docker",
             "run",
-            # The mounted socket allows access to docker
+            # the mounted socket allows access to docker
             "-v",
             "/var/run/docker.sock:/var/run/docker.sock",
-            # Network allows access to BOPTEST API in other containers
+            # automatically remove container when stopped/exited
+            "--rm",
+            # network allows access to BOPTEST API in other containers
             "--network",
             "beobench-net",
             "--name",
