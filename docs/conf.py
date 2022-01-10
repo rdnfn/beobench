32673d19d4b1ddea596d0da8c65c3ed7674b05f1
rdnfn <75615911+rdnfn@users.noreply.github.com>
2022-01-10 11:12:39 +0000

    docs: add autogenerated API docs

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,11 @@ import beobench  # pylint: disable=wrong-import-position
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.autosummary",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
