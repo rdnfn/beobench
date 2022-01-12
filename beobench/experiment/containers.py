cb5cc957dacec3e6d1b2db26fa6060c78eb535af
rdnfn <75615911+rdnfn@users.noreply.github.com>
2022-01-12 16:37:33 +0000

    feat: add build context to containers function

--- beobench/experiment/containers.py
+++ beobench/experiment/containers.py
@@ -4,24 +4,48 @@ import subprocess
 import os
 
 
-def build_experiment_container(use_no_cache: bool = False) -> None:
-    """Build experiment container from beobench/integrations/boptest/Dockerfile."""
+def build_experiment_container(
+    build_context: str, docker_tag: str = None, use_no_cache: bool = False
+) -> None:
+    """Build experiment container from beobench/integrations/boptest/Dockerfile.
 
-    print("Building experiment container ...")
+    Args:
+        build_context (str): context to build from. This can either be a path to
+            directory with Dockerfile in it, or a URL to a github repo, or name
+            of existing beobench integration (e.g. `boptest`). See the official docs
+            https://docs.docker.com/engine/reference/commandline/build/ for more info.
+        use_no_cache (bool, optional): wether to use cache in build. Defaults to False.
+    """
 
     flags = []
     if use_no_cache:
         flags.append("--no-cache")
 
+    print(f"Building experiment container `{docker_tag}`...")
+
+    AVAILABLE_INTEGRATIONS = ["boptest"]  # pylint: disable=invalid-name
+
+    if build_context in AVAILABLE_INTEGRATIONS:
+        docker_tag = f"beobench_{build_context}:latest"
+        build_context = (
+            f"https://github.com/rdnfn/"
+            f"beobench.git#:beobench/integrations/{build_context}"
+        )
+    else:
+        if docker_tag is None:
+            # get alphanumeric name from context
+            context_name = "".join(e for e in build_context if e.isalnum())
+            docker_tag = f"beobench_custom_{context_name}:latest"
+
     args = [
         "docker",
         "build",
         "-t",
-        "beobench-experiment:latest",
+        docker_tag,
         "-f",
         "Dockerfile",  # change to non-default name
         *flags,
-        "https://github.com/rdnfn/beobench.git#:beobench/integrations/boptest",
+        build_context,
     ]
     env = os.environ.copy()
     env["DOCKER_BUILDKIT"] = "0"
