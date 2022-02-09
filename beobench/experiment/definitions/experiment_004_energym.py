71d62089b7c2381422f2962f3fad5f5c2f76fee6
rdnfn <75615911+rdnfn@users.noreply.github.com>
2022-02-09 15:54:45 +0000

    feat: update experiment definitions

--- beobench/experiment/definitions/experiment_004_energym.py
+++ beobench/experiment/definitions/experiment_004_energym.py
@@ -7,17 +7,23 @@ from beobench.experiment.definitions.default import problem, method, rllib_setup
 problem = {
     "name": "sinergym_test_problem",
     "description": ("Control problem corresponding to " "created by sinergym env ''."),
-    "problem_library": "sinergym",
+    "problem_library": "https://github.com/rdnfn/beobench_contrib.git#dev/energym-integration:gyms/energym",
     "rllib_experiment_config": {
         "config": {
-            "env": "sinergym-Eplus-5Zone-hot-continuous-v1",
+            "env": "MixedUseFanFCU-v0",
             "env_config": {
-                "name": "Eplus-5Zone-hot-continuous-v1",
-                "normalize": True,
+                "energym_environment": "MixedUseFanFCU-v0",
+                "weather": "GRC_A_Athens",
+                "days": 365,
+                "gym_kwargs": {
+                    "max_episode_length": 35040,
+                    "step_period": 15,
+                    "normalize": True,
+                },
             },
             "gamma": 0.999,
-            "output": "/root/ray_results/debug/",
-            "output_compress_columns": [],
+            # "output": "/root/ray_results/debug/",
+            # "output_compress_columns": [],
             "batch_mode": "complete_episodes",
             "horizon": 1000,
             "metrics_smoothing_episodes": 5,
@@ -32,7 +38,7 @@ rllib_setup = {
             # Utilities settings
             "framework": "torch",
             "log_level": "WARNING",
-            "num_workers": 8,  # 1 for silent mode, can at least be 6
+            "num_workers": 1,  # 1 for silent mode, can at least be 6
             "num_gpus": 1,
             "seed": ray.tune.randint(0, 10000000),
         },
