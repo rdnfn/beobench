e6cd0a98fd2873b65ae02e69a021f19a50b3dc91
rdnfn <75615911+rdnfn@users.noreply.github.com>
2022-03-06 12:11:39 +0000

    feat: add config to rllib config parser

--- beobench/experiment/config_parser.py
+++ beobench/experiment/config_parser.py
@@ -10,6 +10,9 @@ def parse(config: Union[str, pathlib.Path]) -> dict:
 
     Args:
         config (Union[str, pathlib.Path]): path of yaml file
+
+    Returns:
+        dict: config in dictionary
     """
 
     # load config dict if path given
@@ -17,9 +20,33 @@ def parse(config: Union[str, pathlib.Path]) -> dict:
         # make sure config is a real path
         if isinstance(config, str):
             config = pathlib.Path(config)
-        with open(config) as config_file:
+        with open(config, "r", encoding="utf-8") as config_file:
             config_dict = yaml.safe_load(config_file)
     else:
         config_dict = config
 
     return config_dict
+
+
+def create_rllib_config(config: dict) -> dict:
+    """Create a configuration for ray.tune.run() method.
+
+    Args:
+        config (dict): beobench config
+
+    Raises:
+        ValueError: this is raised if the config does not specify an rllib agent.
+
+    Returns:
+        dict: kwargs for ray.tune.run() method
+    """
+
+    # Check if config is with rllib agent
+    if config["agent"]["origin"] is not "rllib":
+        raise ValueError("Configuration does not have rllib agent origin set.")
+
+    rllib_config = config["agent"]["config"]
+    rllib_config["config"]["env_config"] = config["env"]["config"]
+    rllib_config["config"]["env"] = config["env"]["name"]
+
+    return rllib_config
