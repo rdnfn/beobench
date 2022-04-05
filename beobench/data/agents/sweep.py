bfcd28df05565d84faf736d1fadb56a13c68fbb2
rdnfn <75615911+rdnfn@users.noreply.github.com>
2022-04-05 12:16:01 +0000

    feat: add wandb sweep agent

---
+++ beobench/data/agents/sweep.py
@@ -0,0 +1,39 @@
+"""Script to run wandb sweeps via Beobench."""
+
+import wandb
+import beobench
+import beobench.experiment.provider
+import beobench.experiment.config_parser
+import beobench.utils
+
+# Get the base config of experiment
+base_config = beobench.experiment.provider.config["agent"]["config"]["base_config"]
+base_config = beobench.experiment.config_parser.parse(base_config)
+
+
+def sweep_func():
+
+    wandb.init()
+
+    # loading config params from sweep and combining them with base config
+    tmp_config = dict(wandb.config)
+    tmp_config = beobench.utils.merge_dicts(
+        a=base_config,
+        b=tmp_config,
+        let_b_overrule_a=True,
+    )
+    beobench.run(
+        config=tmp_config,
+        no_additional_container=True,
+    )
+
+
+# Setting up sweep config
+sweep_config = beobench.experiment.provider.config["agent"]["config"]["sweep_config"]
+sweep_config["program"] = "beobench"
+
+# Starting sweep
+sweep_id = wandb.sweep(sweep_config)
+
+# Creating agent (for sweep)
+wandb.agent(sweep_id, function=sweep_func)
