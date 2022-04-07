3b8bc2827e86fc1cc064696637f6d2c833de67c8
rdnfn <75615911+rdnfn@users.noreply.github.com>
2022-04-07 08:36:46 +0000

    exp: update wandb sweep script

--- beobench/data/agents/sweep.py
+++ beobench/data/agents/sweep.py
@@ -13,15 +13,18 @@ base_config = beobench.experiment.config_parser.parse(base_config)
 
 def sweep_func():
 
-    wandb.init()
+    wandb.init(dir="/root/ray_results/")
+    print("initialised wandb inside sweep func.")
 
     # loading config params from sweep and combining them with base config
     tmp_config = dict(wandb.config)
+    print("Config given by wandb", tmp_config)
     tmp_config = beobench.utils.merge_dicts(
         a=base_config,
         b=tmp_config,
         let_b_overrule_a=True,
     )
+    print("Config given by sweep:", tmp_config)
     beobench.run(
         config=tmp_config,
         no_additional_container=True,
