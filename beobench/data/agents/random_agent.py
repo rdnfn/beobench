a405db8d0fe3b0f1f676056b4baa61085df1385c
rdnfn <75615911+rdnfn@users.noreply.github.com>
2022-04-08 12:29:08 +0000

    feat: add wandb experiment tracking to random agent script

--- beobench/data/agents/random_agent.py
+++ beobench/data/agents/random_agent.py
@@ -1,17 +1,58 @@
 """Random agent for testing beobench experiment containers"""
 
-import beobench.experiment.provider
+from beobench.experiment.provider import config, create_env
+import wandb
+import numpy as np
 
-print("Random agent: creating env.")
-env = beobench.experiment.provider.create_env()
-print("Random agent: resetting env.")
+# Setting up experiment tracking via wandb
+wandb_used = config["general"]["wandb_project"] is not None
+if wandb_used:
+    wandb.init(
+        project=config["general"]["wandb_project"],
+        entity=config["general"]["wandb_entity"],
+        group=config["general"]["wandb_group"],
+        name="random_agent_" + wandb.util.generate_id(),
+    )
+
+# Determining length of experiment (if set)
+try:
+    num_timesteps = config["agent"]["config"]["stop"]["timesteps_total"]
+except KeyError:
+    num_timesteps = 10
+
+# Determine max length of an episode
+try:
+    horizon = config["agent"]["config"]["config"]["horizon"]
+except KeyError:
+    horizon = 1000
+
+
+print("Random agent: starting test.")
+
+env = create_env()
 observation = env.reset()
-for _ in range(10):
-    print("Random agent: taking action.")
+
+num_steps_per_ep = 0
+episode = 0
+ep_rewards = []
+
+for _ in range(num_timesteps):
+    episode += 1
+    num_steps_per_ep += 1
+
     action = env.action_space.sample()
-    print(action)
     observation, reward, done, info = env.step(action)
-    if done:
-        observation = env.reset()
+
+    ep_rewards.append(reward)
+
+    if done or num_steps_per_ep >= horizon:
+
+        if wandb_used:
+            wandb.log({"episode_reward_mean": np.sum(ep_rewards), "step": episode})
+
+        num_steps_per_ep = 0
+        ep_rewards = []
+        if done:
+            observation = env.reset()
 env.close()
 print("Random agent: completed test.")
