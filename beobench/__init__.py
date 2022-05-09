539d7cd50a06a425ed8a5f9c42c688edc3ff7bc5
rdnfn <75615911+rdnfn@users.noreply.github.com>
2022-05-09 14:11:40 +0000

    Bump version: 0.4.3 → 0.4.4

--- beobench/__init__.py
+++ beobench/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Beobench authors"""
 __email__ = "-"
-__version__ = "0.4.3"
+__version__ = "0.4.4"
 
 from beobench.utils import restart
 from beobench.experiment.scheduler import run
