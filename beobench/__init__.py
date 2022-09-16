15088be5b2fd41cac811bf3c865da8e3e0e86a52
rdnfn <75615911+rdnfn@users.noreply.github.com>
2022-09-16 14:51:58 +0000

    feat: add explicit windows warning if executed on that os (#91)

--- beobench/__init__.py
+++ beobench/__init__.py
@@ -4,5 +4,20 @@ __author__ = """Beobench authors"""
 __email__ = "-"
 __version__ = "0.5.2"
 
+import os
+from loguru import logger
+
 from beobench.utils import restart
 from beobench.experiment.scheduler import run
+
+# throw error message if trying to use Beobench on windows
+if os.name == "nt":
+    logger.critical(
+        "ERROR: you appear to try to run Beobench directly on Windows. "
+        "Beobench CANNOT be run directly on Windows. \n\n"
+        "However, you can run Beobench inside Windows Subsystem for Linux (WSL)"
+        " on a Windows machine. "
+        "See the following link: "
+        "https://docs.microsoft.com/en-us/windows/wsl/install."
+        "Beobench will still execute initially but will fail eventually."
+    )
